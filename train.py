951f34501ad2911996b46d92cad39d737555f9d4
Nidaaa <nidashaikh16@eng.rizvi.edu.in>
2023-07-09 22:31:55 +0530

    Craeting training data

--- train.py
+++ train.py
@@ -1,13 +1,16 @@
 import json
 import string
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset,DataLoader
 from nltk_utils import tokenize, stem, create_bag_of_words 
+
+# Hyperparameters
+BATCH_SIZE = 8
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
-    
-# print(intents)
-
 all_words = []
 tags = []
 xy = []    # will later hold all our oatterns and tags
@@ -26,7 +29,33 @@ all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
 
+X_train = []
+y_train = []
+for (tokens, tag) in xy :   # Unpacking the tuple
+    bag = create_bag_of_words(tokens,all_words)
+    X_train.append(bag)
+    
+    tag = tags.index(tag)
+    y_train.append(tag)
+    
+X_train = np.array(X_train)
+y_train = np.array(y_train)
 
-
-
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+        
+    # dataset[idx]
+    def __getitem__(self,index):
+        return self.x_data[index],self.y_data[index]
+    
+    def __len__(self):
+        return self.n_samples
+    
+dataset = Dataset()
+data_loader = DataLoader(dataset=dataset, batch_size=BATCH_SIZE, num_workers=2)
+    
+    
 
